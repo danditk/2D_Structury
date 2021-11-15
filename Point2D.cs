dc1ee041574bba9e8132d269375d9feb4ca6b9f3
Darek <darekt@abmicro.pl>
2021-11-15 20:33:28 +0100

    Init struct Point2D

---
+++ Point2D.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_Structury
+{
+    struct Point2D
+    {
+        
+    }
+}
